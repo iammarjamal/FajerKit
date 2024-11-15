2d22a67b3d0c14d0558a6b7f60427aeaffd93f9c
iammarjamal <ammar.jamal.jadallah@gmail.com>
2024-11-15 16:54:45 +0200

    update packages

--- config/fajerkit.php
+++ config/fajerkit.php
@@ -2,5 +2,50 @@
 
 // config for Iammarjamal/FajerKit
 return [
+    'laravel' => [
 
+        // Livewire: A full-stack framework for Laravel that simplifies building dynamic UIs.
+        // More info: https://livewire.laravel.com/
+        'livewire' => true,
+
+        // Volt: An elegantly crafted functional API for Livewire.
+        // More info: https://github.com/livewire/volt
+        'volt' => true,
+
+        // Laravel Folio: A powerful page based router designed to simplify routing in Laravel applications.
+        // More info: https://laravel.com/docs/11.x/folio
+        'laravel-folio' => true,
+
+        // Devdojo Auth: DevDojo Auth is an open-source composer package that adds customizable authentication screens to your Laravel application.
+        // More info: https://devdojo.com/auth/
+        'devdojo-auth' => true,
+
+        // CamelUI: A simple, beautiful, and versatile UI inspired by the Arabian desert.
+        // More info: https://camelui.dev/
+        'camelui' => true,
+
+        // Laravel PWA: This Laravel package turns your project into a progressive web app.
+        // More info: https://github.com/silviolleite/laravel-pwa
+        'laravel-pwa' => true,
+
+        // Browser Detect: Easy to use package to identify the visitor's browser details and device type.
+        // More info: https://github.com/hisorange/browser-detect
+        'browser-detect' => true,
+
+        // Laravel Page Speed: Package to optimize your site automatically which results in a 35%+ optimization.
+        // More info: https://github.com/renatomarinho/laravel-page-speed
+        'laravel-page-speed' => true,
+
+        '',
+
+    ],
+
+    // Javascript Packages (Node)
+    'node' => [
+
+        // TailwindCSS: A utility-first CSS framework for rapidly building custom designs.
+        // More info: https://tailwindcss.com/
+        'tailwindcss' => true,
+
+    ],
 ];
--- src/Commands/FajerKitCommand.php
+++ src/Commands/FajerKitCommand.php
@@ -3,17 +3,164 @@
 namespace Iammarjamal\FajerKit\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class FajerKitCommand extends Command
 {
-    public $signature = 'fajerkit';
-
-    public $description = 'My command';
+    public $signature = 'fajerkit:install';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        /* Laravel Composer Install */
+        /*
+        ** You can use the following function like this:
+        ** composerInstall('package_name_in_lower_case', 'composer_package', 'artisan_command_without_php_artisan')
+        ** You don't have to edit this function. You can edit the config file before running 'php artisan fajerkit:install'
+        */
+    
+        // Livewire Installation
+        $this->composerInstall('livewire', 'livewire/livewire', 'livewire:publish --config');
+    
+        // Volt Installation
+        $this->composerInstall('volt', 'livewire/volt', 'volt:install');
 
+        // Laravel Folio Installation
+        $this->composerInstall('laravel-folio', 'laravel/folio', 'folio:install');
+
+        // Devdojo Auth Installation
+        $this->composerInstall('devdojo-auth', 'devdojo/auth', 'vendor:publish --tag="auth:assets,auth:config,auth:ci,auth:migrations"');
+
+        // CamelUI Installation
+        $this->composerInstall('camelui', 'iammarjamal/camelui', 'vendor:publish --tag="camelui-config"');
+    
+        // Laravel PWA Installation
+        $this->composerInstall('laravel-pwa', 'silviolleite/laravelpwa --prefer-dist', 'vendor:publish --provider="LaravelPWA\Providers\LaravelPWAServiceProvider"');
+    
+        // Browser Detect Installation
+        $this->composerInstall('browser-detect', 'hisorange/browser-detect', '');
+    
+
+        
+        /* Node npm Install */
+        /*
+        ** You can use the following function for npm packages like this:
+        ** composerInstall('package_name_in_lower_case', 'npm_command', 'optional_additional_command')
+        ** You don't have to edit this function. You can edit the config file before running 'php artisan fajerkit:install'
+        */
+    
+        // TailwindCSS Installation
+        $this->nodeInstall(
+            'tailwindcss',
+            'npm install tailwindcss postcss autoprefixer',
+            'npx tailwindcss init -p && npm install -D @tailwindcss/forms && npm install -D @tailwindcss/typography && echo \'const colors = require("tailwindcss/colors"); 
+        module.exports = {
+          darkMode: "class",
+          content: [
+            "./resources/**/*.blade.php",
+            "./resources/**/*.js",
+            "./resources/**/*.vue",
+            "./resources/**/*.jsx",
+            "./app/**/*.php",
+            "./resources/**/*.{php,html,js,jsx,ts,tsx,vue,twig}",
+          ],
+          theme: {
+            // Screens
+            screens: {
+              sm: "320px",
+              md: "768px",
+              lg: "1024px",
+              xl: "1366px",
+            },
+            // Extend
+            extend: {
+              // Colors
+              colors: {
+                primary: colors.blue,
+                secondary: colors.gray,
+                success: colors.emerald,
+                danger: colors.red,
+                warning: colors.amber,
+                info: colors.blue,
+                light: colors.white,
+                dark: colors.black
+              },
+              // Animations
+              keyframes: {
+                shimmer: {
+                  "100%": { transform: "translateX(100%)" },
+                },
+              },
+            },
+          },
+          plugins: [
+            require("@tailwindcss/forms"),
+            require("@tailwindcss/typography"),
+          ],
+        }\' > tailwind.config.js'
+        );        
+        
         return self::SUCCESS;
+    }
+    
+    /**
+     * Install a package using Composer and run an optional Artisan command.
+     *
+     * @param string $packageName The name of the package (in lower case).
+     * @param string $composerCommand The composer command to install the package.
+     * @param string $artisanCommand Optional artisan command to run after installation.
+    */
+
+    private function composerInstall(string $packageName, string $composerCommand, string $artisanCommand = '')
+    {
+        if (config("fajerkit.laravel.$packageName")) {
+            // Run Composer Command
+            exec('composer require '.$composerCommand, $output, $returnVar);
+            echo implode("\n", $output) . "\n";
+    
+            // Success or Failure message
+            echo $returnVar === 0 
+                ? "$packageName has been installed successfully." 
+                : "Failed to install $packageName. Exit Code: $returnVar";
+    
+            // Run Artisan Command if installation was successful
+            if ($returnVar === 0 && $artisanCommand) {
+                exec('php artisan  '.$artisanCommand, $output, $returnVar);
+                echo implode("\n", $output) . "\n";
+                
+                echo $returnVar === 0 
+                ? "$packageName has been installed successfully." 
+                : "Failed to artisan $packageName. Exit Code: $returnVar";
+            }
+        }
+    }
+    
+    /**
+     * Install a Node package via npm.
+     *
+     * @param string $packageName The name of the package (in lower case).
+     * @param string $npmCommand The npm command to install the package.
+     * @param string $additionalCommand Optional additional npm commands to run.
+    */
+
+    private function nodeInstall(string $packageName, string $npmCommand, string $additionalCommand = '')
+    {
+        if (config("fajerkit.node.$packageName")) {
+            // Run npm command
+            exec($npmCommand, $output, $returnVar);
+            echo implode("\n", $output) . "\n";
+    
+            // Success or Failure message
+            echo $returnVar === 0 
+                ? "$packageName has been installed successfully via npm." 
+                : "Failed to install $packageName via npm. Exit Code: $returnVar";
+    
+            // Run additional npm command if provided
+            if ($returnVar === 0 && $additionalCommand) {
+                exec($additionalCommand, $output, $returnVar);
+                echo implode("\n", $output) . "\n";
+            }
+        }
     }
 }
